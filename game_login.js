37ff2df95941a351121baf2b44c94e09ad44035c
samarth2010 <76905546+samarth2010@users.noreply.github.com>
2021-05-16 12:04:19 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,13 @@
+
+localStorage.removeItem("player1_name")
+localStorage.removeItem("player2_name")
+
+function add_User()
+{
+ player1_name = document.getElementById("player1_name_input").value;
+ player2_name = document.getElementById("player2_name_input").value;
+ localStorage.setItem("player1_name" , player1_name );
+ localStorage.setItem("player2_name" , player2_name );
+
+ window.location="game.html";
+}
